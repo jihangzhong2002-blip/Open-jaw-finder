5ccec6a6b239eade173e0531345dea229e11e98a
jihangzhong2002-blip <jihangzhong2002@gmail.com>
2026-05-26 17:03:27 +0800

    Change export syntax from ES6 to CommonJS

--- api/search.js
+++ api/search.js
@@ -1,7 +1,7 @@
 const cache = { data: null, timestamp: 0 };
 const CACHE_TTL = 6 * 60 * 60 * 1000; // 6小时
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   const { mode, from, to, date, returnDate } = req.query;
